f85c11811d3c9e8219aa1b60435c66034ea21cf7
unknown <khieu.nv05@gmail.com>
2024-07-18 14:36:47 +0700

    test git

---
+++ src/main/java/com/vti/LoginAndWeb/LoginAndWebApplication.java
@@ -0,0 +1,13 @@
+package com.vti.LoginAndWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/LoginAndWeb/LoginAndWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.LoginAndWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
