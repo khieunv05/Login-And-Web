05e32fc68a370c3015092cfbf28ad42993f87b3d
unknown <khieu.nv05@gmail.com>
2024-07-18 15:03:36 +0700

    [Add] user

---
+++ src/main/java/com/vti/LoginAndWeb/Entity/user.java
@@ -0,0 +1,33 @@
+package com.vti.LoginAndWeb.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username",length = 10,nullable = false,unique = true)
+    private String username;
+
+    @Column(name = "password", length = 50,nullable = false)
+    private String password;
+
+    @Column(name = "created_at",updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+}
